ad4c57c030342e20eb887a74f86a5e7c51d03ff0
karlos <karloszuru@gmail.com>
2024-09-03 23:17:02 -0500

    basic book spec test.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,25 @@
+# spec/models/book_spec.rb
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      price: 19.99,
+      published_date: '2022-01-01'
+    )
+  end
+
+  context 'validations' do
+    it 'is valid with a title' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      subject.title = nil
+      expect(subject).not_to be_valid
+    end
+
+  end
 end
