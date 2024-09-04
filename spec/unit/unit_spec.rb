cc39f56be95a701920c65720b5c148eb311f281a
karlos <karloszuru@gmail.com>
2024-09-03 23:17:12 -0500

    numeral tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Book, type: :model do
       title: 'Harry Potter',
       author: 'J.K. Rowling',
       price: 19.99,
-      published_date: '2024-01-01'
+      published_date: '1900-01-01'
     )
   end
 
@@ -34,4 +34,25 @@ RSpec.describe Book, type: :model do
     subject.published_date = nil
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid with a non-numerical price' do
+    subject.price = 'abc'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is valid with a positive numerical price' do
+    subject.price = 29.99
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid with a negative price' do
+    subject.price = -19.99
+    expect(subject).not_to be_valid
+  end
+
+  it 'is valid with a price of zero' do
+    subject.price = 0.0
+    expect(subject).to be_valid
+  end
+
 end
