2ce4a89c543b253696d620b05246f62f3d0fb6f8
karlos <karloszuru@gmail.com>
2024-09-03 23:16:40 -0500

    validates :price, numericality: { greater_than_or_equal_to: 0 }

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,4 @@
 class Book < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 100 }
-  # :author, :price, :published_date, presence: true
-  # validates :price, numericality: true
+  validates :title, :author, :price, :published_date, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
